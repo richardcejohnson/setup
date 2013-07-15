2b1c0742d00b2753ae89414bb61cdba780b52409
Richard Johnson <spoondoctor@googlemail.com>
2013-07-15 23:02:46 +0100

    adding logger to the user data setup file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/bash -ex
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
 # for headless setup. 
 
